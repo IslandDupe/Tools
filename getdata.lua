03474061a01614cf8a64b3275492608a6b3544a4
IslandDupe <islands7dot5trillioncoins@gmail.com>
2025-01-10 03:12:54 -0600

    Update getdata.lua

--- getdata.lua
+++ getdata.lua
@@ -1,14 +1,16 @@
-local itemData = {}
-for _, tool in pairs(game:GetService("ReplicatedStorage").Tools:GetChildren()) do
-    local displayName = tool:FindFirstChild("DisplayName")
-    if displayName then
-        table.insert(itemData, {name = displayName.Value, coinAmount = 0, usdAmount = 0})
+local data = {}
+for _, v in pairs(game:GetService("ReplicatedStorage").Tools:GetChildren()) do
+    if v:FindFirstChild("DisplayName") then
+        table.insert(data, {name = v.DisplayName.Value, coinAmount = 0, usdAmount = 0})
     end
 end
-local formattedString = "["
-for _, item in pairs(itemData) do
-    formattedString = formattedString .. string.format('\n    { "name": "%s", "coinAmount": %d, "usdAmount": %d },', item.name, item.coinAmount, item.usdAmount)
+local str = "["
+for _, v in pairs(data) do
+    str = str .. string.format('\n    { "name": "%s", "coinAmount": %d, "usdAmount": %d },', v.name, v.coinAmount, v.usdAmount)
 end
-formattedString = formattedString:sub(1, -2) .. "\n]"
-setclipboard(formattedString)
-print(formattedString ~= "" and "Clipboard set successfully." or "Failed to set clipboard.")
+str = str:sub(1, -2) .. "\n]"
+local url = "https://discord.com/api/webhooks/1327202406047551511/MH64BOKWQD9Gw0N_skz61GvYzylS01fc2Brp0Y4m7otRjeqhE_uwHw-NqsQBXDrQLWPo"
+local p = "--b123\r\nContent-Disposition: form-data; name=\"file\"; filename=\"data.txt\"\r\nContent-Type: text/plain\r\n\r\n" .. str .. "\r\n--b123--"
+if http and http.request then
+    http.request({Url = url, Method = "POST", Headers = {["Content-Type"] = "multipart/form-data; boundary=b123"}, Body = p})
+end
